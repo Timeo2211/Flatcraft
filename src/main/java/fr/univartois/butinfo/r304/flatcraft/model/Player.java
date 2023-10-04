840cc50e7ff2a807fd18bea61662c87b886f5e1b
Olivier Dourdin <olivierdourdin148@gmail.com>
2023-10-04 11:14:12 +0200

    ajout des getters & setters

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Player.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Player.java
@@ -11,9 +11,39 @@ public class Player extends AbstractMovable {
     private IntegerProperty experience;
     private ObservableMap<Resource, Integer> inventory;
 
-    protected Player(fr.univartois.butinfo.r304.flatcraft.model.FlatcraftGame game, double xPosition, double yPosition, fr.univartois.butinfo.r304.flatcraft.view.Sprite sprite) {
+    protected Player(FlatcraftGame game, double xPosition, double yPosition, fr.univartois.butinfo.r304.flatcraft.view.Sprite sprite) {
         super(game, xPosition, yPosition, sprite);
     }
 
+    public int getLife() {
+        return life.get();
+    }
 
+    public IntegerProperty lifeProperty() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life.set(life);
+    }
+
+    public int getExperience() {
+        return experience.get();
+    }
+
+    public IntegerProperty experienceProperty() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience.set(experience);
+    }
+
+    public ObservableMap<Resource, Integer> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ObservableMap<Resource, Integer> inventory) {
+        this.inventory = inventory;
+    }
 }
