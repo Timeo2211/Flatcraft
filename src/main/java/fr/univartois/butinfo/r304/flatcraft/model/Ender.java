dd6f860453c79a9459be196e49480d8a2b278d66
Vallet Alexandre <alexandre_vallet@ens.univ-artois.fr>
2023-11-19 01:03:06 +0100

    ajout de la methode createMob

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Ender.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Ender.java
@@ -4,53 +4,45 @@ import fr.univartois.butinfo.r304.flatcraft.model.map.MyCell;
 import fr.univartois.butinfo.r304.flatcraft.view.ISpriteStore;
 import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
 
-public class OverWorld implements CellFactory {
-
+public class Ender implements CellFactory{
     private ISpriteStore spriteStore;
 
-    @Override
     public void setSpriteStore(ISpriteStore spriteStore) {
         this.spriteStore = spriteStore;
     }
 
-    @Override
     public Cell createSky() {
         Sprite sprite = spriteStore.getSprite("sky");
         MyCell cell= new MyCell(sprite);
-       return cell;
+        return cell;
     }
 
-    @Override
     public Cell createSoilSurface() {
-        return new MyCell(spriteStore.getSprite("grass"));
+        return new MyCell(spriteStore.getSprite("sandstone_brick"));
     }
 
-    @Override
     public Cell createSubSoil() {
-        return new MyCell(spriteStore.getSprite("dirt"));
-    }
-
-    @Override
-    public Cell createTrunk() {
-        return new MyCell(spriteStore.getSprite("trunk"));
-    }
-
-    @Override
-    public Cell createLeaves() {
-        return new MyCell(spriteStore.getSprite("leaves_simple"));
-    }
-
-    @Override
-    public Cell createTerril() {
         return new MyCell(spriteStore.getSprite("sand"));
+    }
+
+    public Cell createTrunk() {
+        return null;
+    }
+
+    public Cell createLeaves() {
+        return null;
+    }
+
+    public Cell createTerril() {
+        return null;
     }
 
     @Override
     public Cell createTree() {
-        return new MyCell(spriteStore.getSprite("tree"));
+        return null;
     }
 
-
+    @Override
     public Cell createMob() {
         return new MyCell(spriteStore.getSprite("tool_diamondsword"));
     }
--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Nether.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Nether.java
@@ -49,4 +49,9 @@ public class Nether implements CellFactory{
     public Cell createTree() {
         return new MyCell(spriteStore.getSprite("tree"));
     }
+
+    @Override
+    public Cell createMob() {
+        return new MyCell(spriteStore.getSprite("tool_diamondsword"));
+    }
 }
