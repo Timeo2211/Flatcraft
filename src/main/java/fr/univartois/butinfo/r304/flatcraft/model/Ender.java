f4419dcda87f12fa32c6d8df2c51c80e3c5fe5a8
Vallet Alexandre <alexandre_vallet@ens.univ-artois.fr>
2023-11-19 18:34:59 +0100

    ajout de la méthode createRessource

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Ender.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/Ender.java
@@ -43,7 +43,17 @@ public class Ender implements CellFactory{
     }
 
     @Override
+    public Cell createWater() {
+        return null;
+    }
+
+    @Override
     public Cell createMob() {
         return new MyCell(spriteStore.getSprite("tool_diamondsword"));
+    }
+
+    @Override
+    public Cell createRessource(int i) {
+        return new MyCell(spriteStore.getSprite("mineral_diamond"));
     }
 }
