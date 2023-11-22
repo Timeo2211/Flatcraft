37f915491d7b44dfd96dd835ff175263779124c1
Olivier Dourdin <olivierdourdin148@gmail.com>
2023-11-22 13:43:15 +0100

    Ajout de la class pour le mouvement Random

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/mobsmouvement/RandomMovement.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/mobsmouvement/RandomMovement.java
@@ -3,7 +3,7 @@ package fr.univartois.butinfo.r304.flatcraft.model.mobsmouvement;
 import fr.univartois.butinfo.r304.flatcraft.model.AbstractMob;
 import java.util.Random;
 
-public class RandomMouvement implements IMobsMouvement {
+public class RandomMovement implements IMobsMovement{
     @Override
     public void move(AbstractMob abstractMob, long deltaTime) {
         Random random = new Random();
