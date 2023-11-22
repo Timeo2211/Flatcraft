1a38459a462e747129a16d516a7b84afeb323cfa
Olivier Dourdin <olivierdourdin148@gmail.com>
2023-11-22 14:07:08 +0100

    Possibilite de bouger le mob dans overworld

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/OverworldMob.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/OverworldMob.java
@@ -1,2 +1,21 @@
-package fr.univartois.butinfo.r304.flatcraft.model;public class OverworldMob {
+package fr.univartois.butinfo.r304.flatcraft.model;
+
+import fr.univartois.butinfo.r304.flatcraft.model.mobsmouvement.IMobsMovement;
+import fr.univartois.butinfo.r304.flatcraft.view.*;
+import fr.univartois.butinfo.r304.flatcraft.model.mobsmouvement.*;
+
+public class OverworldMob extends AbstractMob{
+    public OverworldMob(FlatcraftGame game, double xPosition, double yPosition, Sprite sprite, IMobsMovement movement) {
+        super(game, xPosition, yPosition, new SpriteStore().getSprite("nc_front"), movement);
+    }
+
+    public OverworldMob(FlatcraftGame game, double xPosition, double yPosition, Sprite sprite, int movement) {
+        super(game, xPosition, yPosition, new SpriteStore().getSprite("nc_front"), null);
+        switch (movement) {
+            case 0 : this.setMovement(new LinearMovement()); System.out.println("Linear"); break;
+            case 1 : this.setMovement(new RandomMovement()); System.out.println("Random"); break;
+            case 2 : this.setMovement(new SmartMovement()); System.out.println("Smart"); break;
+            default: break;
+        }
+    }
 }
