9d1178c0b913b6a16e2338865842da6caf92a247
Olivier Dourdin <olivierdourdin148@gmail.com>
2023-11-22 14:04:35 +0100

    Possibilite de bouger le mob dans le nether

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/NetherMob.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/NetherMob.java
@@ -1,2 +1,11 @@
-package fr.univartois.butinfo.r304.flatcraft.model;public class NetherMob {
+package fr.univartois.butinfo.r304.flatcraft.model;
+
+import fr.univartois.butinfo.r304.flatcraft.model.mobsmouvement.IMobsMovement;
+import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
+import fr.univartois.butinfo.r304.flatcraft.view.SpriteStore;
+
+public class NetherMob extends AbstractMob {
+    public NetherMob(FlatcraftGame game, double xPosition, double yPosition, Sprite sprite, IMobsMovement movement) {
+        super(game, xPosition, yPosition, new SpriteStore().getSprite("lava"), movement);
+    }
 }
