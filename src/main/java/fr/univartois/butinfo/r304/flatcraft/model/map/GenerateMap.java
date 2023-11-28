f818d9f2476e643a6837aa6ee8a861c19196702d
Vallet Alexandre <alexandre_vallet@ens.univ-artois.fr>
2023-11-28 14:05:48 +0100

    ajout de la dureté dans GenerateMap

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateMap.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/map/GenerateMap.java
@@ -1,11 +1,17 @@
 package fr.univartois.butinfo.r304.flatcraft.model.map;
 
+import fr.univartois.butinfo.r304.flatcraft.model.Cell;
 import fr.univartois.butinfo.r304.flatcraft.model.CellFactory;
 import fr.univartois.butinfo.r304.flatcraft.model.GameMap;
-import java.util.Random;
+import fr.univartois.butinfo.r304.flatcraft.model.resources.Resource;
+import fr.univartois.butinfo.r304.flatcraft.view.Sprite;
+
+import java.util.*;
 
 public class GenerateMap implements fr.univartois.butinfo.r304.flatcraft.model.IGenerateMap {
 
+    //création d'un dictionnaire contenant les ressources et les cellules associées
+    public static Map<Cell, Resource> resources = new HashMap<>();
     private int height;
     private int width;
     private CellFactory cellFactory;
@@ -31,11 +37,15 @@ public class GenerateMap implements fr.univartois.butinfo.r304.flatcraft.model.I
 
         for(int j = 0; j < width; j++){
             map.setAt(map.getSoilHeight(), j, cellFactory.createSoilSurface());
+            Resource newResource = new Resource("grass", cellFactory.createSubSoil().getSprite(), null, 1);
+            resources.put(map.getAt(map.getSoilHeight(), j), newResource);
         }
 
         for(int i = map.getSoilHeight() + 1; i < height; i++){
             for(int j = 0; j < width; j++){
                 map.setAt(i, j, cellFactory.createSubSoil());
+                Resource newResource = new Resource("dirt", cellFactory.createSubSoil().getSprite(), null, 2);
+                resources.put(map.getAt(i, j), newResource);
             }
         }
 
@@ -46,12 +56,18 @@ public class GenerateMap implements fr.univartois.butinfo.r304.flatcraft.model.I
         for (int i = 0; i < nb; i++) {
             for(int j = 0; j < 5; j++){
                 map.setAt(map.getSoilHeight() - 1, pos+j, cellFactory.createTerril());
+                Resource newResource = new Resource("terril", cellFactory.createTerril().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 1, pos+j), newResource);
             }
             for(int j = 1; j < 4; j++){
                 map.setAt(map.getSoilHeight() - 2, pos+j, cellFactory.createTerril());
+                Resource newResource = new Resource("terril", cellFactory.createTerril().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 2, pos+j), newResource);
             }
             for(int j = 2; j < 3; j++){
                 map.setAt(map.getSoilHeight() - 3, pos+j, cellFactory.createTerril());
+                Resource newResource = new Resource("terril", cellFactory.createTerril().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 3, pos+j), newResource);
             }
             pos = posTerril.nextInt(75);
         }
@@ -68,18 +84,28 @@ public class GenerateMap implements fr.univartois.butinfo.r304.flatcraft.model.I
 
             for (int x = 0; x < 5; x++) {
                 map.setAt(map.getSoilHeight() - 1 - x, posT, cellFactory.createTree());
+                Resource newResource = new Resource("tronc", cellFactory.createTree().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 1 - x, posT), newResource);
             }
             for (int x = 0; x < 7; x++) {
                 map.setAt(map.getSoilHeight() - 6, posT - 3 + x, cellFactory.createLeaves());
+                Resource newResource = new Resource("leaves", cellFactory.createLeaves().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 6, posT - 3 + x), newResource);
             }
             for (int x = 0; x < 5; x++) {
                 map.setAt(map.getSoilHeight() - 7, posT - 2 + x, cellFactory.createLeaves());
+                Resource newResource = new Resource("leaves", cellFactory.createLeaves().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 7, posT - 2 + x), newResource);
             }
             for (int x = 0; x < 3; x++) {
                 map.setAt(map.getSoilHeight() - 8, posT - 1 + x, cellFactory.createLeaves());
+                Resource newResource = new Resource("leaves", cellFactory.createLeaves().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 8, posT - 1 + x), newResource);
             }
             for (int x = 0; x < 1; x++) {
                 map.setAt(map.getSoilHeight() - 9, posT + x, cellFactory.createLeaves());
+                Resource newResource = new Resource("leaves", cellFactory.createLeaves().getSprite(), null, 0);
+                resources.put(map.getAt(map.getSoilHeight() - 9, posT + x), newResource);
             }
         }
 
@@ -100,6 +126,14 @@ public class GenerateMap implements fr.univartois.butinfo.r304.flatcraft.model.I
             }
 
             map.setAt(map.getSoilHeight() + posV, posH-1, cellFactory.createRessource(ressource));
+
+            Cell cell = map.getAt(map.getSoilHeight() + posV, posH-1);
+            Sprite sprite = cell.getSprite();
+            resources.remove(cell);
+            System.out.println(ressource);
+            Resource newResource = new Resource("minerai", cellFactory.createSubSoil().getSprite(), null, ressource);
+            resources.put(cell, newResource);
+
             posH = posRessource.nextInt(74);
             posV = posRessource.nextInt(height - map.getSoilHeight() - 1);
         }
