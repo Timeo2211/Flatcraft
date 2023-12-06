14f9a37b59830607b31a9569b49c06e4b06c8aef
Duchet Timeo <timeo_duchet@ens.univ-artois.fr>
2023-12-06 15:02:30 +0100

    ajout de la classe FurnaceRule

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/FurnaceRule.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/FurnaceRule.java
@@ -1,2 +1,7 @@
-package fr.univartois.butinfo.r304.flatcraft.model.craft;public class FurnaceRule {
+package fr.univartois.butinfo.r304.flatcraft.model.craft;
+
+public class FurnaceRule extends CraftRule {
+    public FurnaceRule(String input, String output, String conditions, int quantity) {
+        super(input, output, conditions, quantity);
+    }
 }
