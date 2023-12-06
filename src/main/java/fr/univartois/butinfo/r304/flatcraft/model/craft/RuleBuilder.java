df3af142fb2d06419d34e5742727a15a147ba9bb
Duchet Timeo <timeo_duchet@ens.univ-artois.fr>
2023-12-06 15:02:55 +0100

    ajout de la classe RuleBuilder

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/RuleBuilder.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/RuleBuilder.java
@@ -1,2 +1,10 @@
-package fr.univartois.butinfo.r304.flatcraft.model.craft;public interface RuleBuilder {
+package fr.univartois.butinfo.r304.flatcraft.model.craft;
+
+// Builder interface
+public interface RuleBuilder {
+    void addInput(String input);
+    void addOutput(String output);
+    void setConditions(String conditions);
+    CraftRule getResult();
+    void setQuantity(int quantity);
 }
