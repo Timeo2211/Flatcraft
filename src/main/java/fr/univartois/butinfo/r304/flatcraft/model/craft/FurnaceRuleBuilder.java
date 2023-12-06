17af90cfb26faf81f57a914ebccf18f49241e110
Duchet Timeo <timeo_duchet@ens.univ-artois.fr>
2023-12-06 15:02:42 +0100

    ajout de la classe FurnaceRuleBuilder

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/FurnaceRuleBuilder.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/FurnaceRuleBuilder.java
@@ -1,2 +1,33 @@
-package fr.univartois.butinfo.r304.flatcraft.model.craft;public class FurnaceRuleBuilder {
+package fr.univartois.butinfo.r304.flatcraft.model.craft;
+
+public class FurnaceRuleBuilder implements RuleBuilder {
+    private String input;
+    private String output;
+    private String conditions;
+    private int quantity;
+
+    @Override
+    public void addInput(String input) {
+        this.input = input;
+    }
+
+    @Override
+    public void addOutput(String output) {
+        this.output = output;
+    }
+
+    @Override
+    public void setConditions(String conditions) {
+        this.conditions = conditions;
+    }
+
+    @Override
+    public CraftRule getResult() {
+        return new FurnaceRule(input, output, conditions, quantity);
+    }
+
+    @Override
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
