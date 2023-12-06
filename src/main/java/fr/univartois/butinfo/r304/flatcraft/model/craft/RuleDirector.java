588828a4c6f9653d323c50a09b5d433570173160
Duchet Timeo <timeo_duchet@ens.univ-artois.fr>
2023-12-06 15:03:10 +0100

    ajout de la classe RuleDirector

--- src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/RuleDirector.java
+++ src/main/java/fr/univartois/butinfo/r304/flatcraft/model/craft/RuleDirector.java
@@ -1,2 +1,21 @@
-package fr.univartois.butinfo.r304.flatcraft.model.craft;public class RuleDirector {
+package fr.univartois.butinfo.r304.flatcraft.model.craft;
+
+public class RuleDirector {
+    private RuleBuilder builder;
+
+    public RuleDirector(RuleBuilder builder) {
+        this.builder = builder;
+    }
+
+    public void constructRule(String input, String output, String conditions, int quantity) {
+        builder.addInput(input);
+        builder.addOutput(output);
+        builder.setConditions(conditions);
+        builder.setQuantity(quantity);
+
+    }
+
+    public CraftRule getRule() {
+        return builder.getResult();
+    }
 }
